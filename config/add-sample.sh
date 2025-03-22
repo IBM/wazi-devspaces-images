bfa512508c2802ed945b21fa4d151b8169f73305
Peter Haumer <4391934+phaumer@users.noreply.github.com>
2025-03-22 12:54:02 -0700

    Added config example for add dashboard tile

---
+++ config/add-sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###############################################################################
+# Licensed Materials - Property of IBM.
+# Copyright IBM Corporation 2025. All Rights Reserved.
+# U.S. Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+###############################################################################
+
+# See <https://docs.redhat.com/en/documentation/red_hat_openshift_dev_spaces/3.19/html/administration_guide/configuring-devspaces#configuring-getting-started-samples> for details
+# Check where Dev Spaces is deployed on your cluster: openshift-operators or openshift-devspaces
+# and adjust the oc command parameter.
+
+sh ./add-sample.sh
+oc create configmap getting-started-samples --from-file=wazi-samples.json -n openshift-operators
+oc label configmap getting-started-samples app.kubernetes.io/part-of=che.eclipse.org app.kubernetes.io/component=getting-started-samples -n openshift-operators
